17eed7be7df2fc2a2983d3b3081bd1a0df490594
oldjunior <anton.b.utkin@gmail.com>
2019-11-22 01:00:47 +0300

    Implement pseudo fetch and debounced search

---
+++ src/ex16_js-async/task-02.js
@@ -0,0 +1,43 @@
+const hints = [
+  'Oliver',
+  'Jack',
+  'Harry',
+  'Jacob',
+  'Abbie',
+  'Daisy',
+  'Gabriella'
+]
+function displayHints() {
+  const
+    searchFieldValue = document.querySelector('.search-field__input').value,
+    hintContainer = document.querySelector('.search-field__hint-container');
+  let hintList, hintsRes;
+
+  hintsRes = hints.filter(hint => searchFieldValue && hint.toLowerCase().startsWith(searchFieldValue.toLowerCase()));
+
+  hintContainer.innerHTML = '';
+  hintList = document.createElement('ul');
+  hintList.className = 'search-field__hint-list';
+  hintContainer.append(hintList);
+
+  hintsRes.forEach(hintRes => {
+    let hint, hintBtn;
+    hint = document.createElement('li');
+    hint.className = 'search-field__hint-list-item';
+    hintList.append(hint);
+
+    hintBtn = document.createElement('button');
+    hintBtn.className = 'search-field__hint-btn';
+    hint.append(hintBtn);
+    hintBtn = document.querySelector('.search-field__hint-btn:last-child');
+    hintBtn.insertAdjacentText('afterbegin', hintRes);
+  });
+}
+function debounce() {
+  let isTimeout = false;
+  if (isTimeout) return;
+  displayHints();
+  isTimeout = true;
+  setTimeout(function() { isTimeout = false }, 500);
+}
+document.querySelector('.search-field__input').addEventListener('input', debounce);
